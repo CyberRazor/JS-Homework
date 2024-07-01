341a4259930e902cc9cbd8c2b23ec83c8fd02651
CyberRazor <stomp224@gmail.com>
2024-07-01 17:49:31 +0100

    add function

--- MikeTaylor_JSHomework.js
+++ MikeTaylor_JSHomework.js
@@ -1,21 +1,25 @@
-let n = 60;
+let n = 15;
 const div3 = "Fizz";
 const div5  = "Buzz";
 const divBoth = "FizzBuzz";
 
-for(let i = 1; i <= n; i++)
-{
-    let by3 = i % 3;
-    let by5 = i % 5;
+function CalculateFizzBuzz(n){
+    for(let i = 1; i <= n; i++)
+    {
+        let by3 = i % 3;
+        let by5 = i % 5;
 
-    if (by3 === 0 && by5 === 0 )
-        {console.log(divBoth);}
+        if (by3 === 0 && by5 === 0 )
+            {console.log(divBoth);}
 
-    else if (by3 === 0)
-        { console.log(div3);}
-    else if (by5 === 0 )
-        {console.log(div5);}
+        else if (by3 === 0)
+            { console.log(div3);}
+        else if (by5 === 0 )
+            {console.log(div5);}
 
-    else
-        {console.log(i);}
+        else
+            {console.log(i);}
+    }
 }
+
+CalculateFizzBuzz(30);
